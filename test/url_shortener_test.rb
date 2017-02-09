e9d876a139335c377c9f9fd7b52f8dbb74e1445d
jb2718 <jbattle2718@gmail.com>
2017-02-08 19:49:42 -0500

    added skeleton of tests

--- test/url_shortener_test.rb
+++ test/url_shortener_test.rb
@@ -11,6 +11,22 @@ require_relative "../url_shortener"
 class URLSHORTENERTest < Minitest::Test
   include Rack::Test::Methods
 
-  def test_index
+  def test_valid_url_shortened
+  end
+
+  def test_url_previously_submitted
+    skip
+  end
+
+  def test_invalid_url_rejected_for_format
+    skip
+  end
+
+  def test_new_short_url_added_to_list
+    skip
+  end
+
+  def test_delete_short_url_list_item
+    skip
   end
 end
